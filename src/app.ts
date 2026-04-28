4b3bf174b566b4919fff27b175791669c8883172
varletint <intvarlet@hotmail.com>
2026-04-28 01:04:23 +0100

    feat: add security and logging middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 import cardRoutes from "./routes/card.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api", cardRoutes);
 app.use(errorHandler);
